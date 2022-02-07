6c533eb06cebb590d180683955ec62921c9e764a
varlamova <varlamvanadia@mail.ru>
2022-02-07 18:13:32 +0300

    Обновила конфигурацию браузера

--- src/test/java/gmail/com/varlamvanadia1996/tests/PracticeFormTests.java
+++ src/test/java/gmail/com/varlamvanadia1996/tests/PracticeFormTests.java
@@ -23,13 +23,16 @@ public class PracticeFormTests {
         String url = System.getProperty("url");
         String browser = System.getProperty("browser", "chrome1");
         String version = System.getProperty("version", "92");
-        String remote = "https://" + login + ":" + password + "@" + url;
-        Configuration.browser = browser;
-        Configuration.browserVersion = version;
-        Configuration.remote = remote;
+
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.browser = browser;
+        Configuration.browserVersion = version;
+        String remote = "https://" + login + ":" + password + "@" + url;
+        Configuration.remote = remote;
 //        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
